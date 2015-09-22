67c9a13191f530cd83ce07bbfb36ee9f1bce2f49
Andrey Gubanov <bookfast.org@gmail.com>
2015-09-22 10:52:37 +0300

    rebuild

--- magic/matreshka-magic.js
+++ magic/matreshka-magic.js
@@ -1,6 +1,6 @@
 ;(function(__root) {
 /*
-	Matreshka Magic v1.1.0-rc2 (2015-09-21), the part of Matreshka project 
+	Matreshka Magic v1.1.0-rc2 (2015-09-22), the part of Matreshka project 
 	JavaScript Framework by Andrey Gubanov
 	Released under the MIT license
 	More info: http://matreshka.io/#magic
