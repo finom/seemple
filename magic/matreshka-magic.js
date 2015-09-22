a3d5658e3b707b84333a7cf849fcde462f3fdbef
Andrey Gubanov <bookfast.org@gmail.com>
2015-09-22 14:49:29 +0300

    rebuild

--- magic/matreshka-magic.js
+++ magic/matreshka-magic.js
@@ -1444,16 +1444,60 @@ matreshka_dir_core_bindings_parsebindings = function (core, sym, initMK, util) {
             }
           }
         }
-      }, all = [], allChildren, i, j, node, bindHTMLKey, atts, attr, attrValue, attrName, keys, key, binder;
+      }, all = [], childNodes, i, j, node, bindHTMLKey, atts, attr, attrValue, attrName, keys, key, binder, previous, textContent, childNode, body;
+    /*for (i = 0; i < nodes.length; i++) {
+    	recursiveSpider(nodes[i]);
+    }*/
     for (i = 0; i < nodes.length; i++) {
-      recursiveSpider(nodes[i]);
+      node = nodes[i];
+      if (node.outerHTML && !~node.outerHTML.indexOf('{{'))
+        continue;
+      childNodes = node.getElementsByTagName('*');
+      for (j = 0; j < childNodes.length; j++) {
+        all.push(childNodes[j]);
+      }
+      all.push(node);
+    }
+    if (!all.length) {
+      return $();
+    }
+    for (j = 0; j < all.length; j++) {
+      node = all[j];
+      if (node.tagName != 'TEXTAREA') {
+        for (i = 0; i < node.childNodes.length; i++) {
+          childNode = node.childNodes[i];
+          previous = childNode.previousSibling;
+          if (childNode.nodeType == 3 && ~childNode.nodeValue.indexOf('{{')) {
+            textContent = childNode.nodeValue.replace(/{{([^}]*)}}/g, '<span mk-html="$1"></span>');
+            try {
+              if (previous) {
+                previous.insertAdjacentHTML('afterend', textContent);
+              } else {
+                node.insertAdjacentHTML('afterbegin', textContent);
+              }
+            } catch (e) {
+              // in case user uses very old webkit-based browser
+              body = document.body;
+              if (previous) {
+                body.appendChild(previous);
+                previous.insertAdjacentHTML('afterend', textContent);
+                body.removeChild(previous);
+              } else {
+                body.appendChild(node);
+                node.insertAdjacentHTML('afterbegin', textContent);
+                body.removeChild(node);
+              }
+            }
+            node.removeChild(childNode);
+          }
+        }
+      }
     }
     for (i = 0; i < nodes.length; i++) {
-      allChildren = nodes[i].querySelectorAll('*');
-      for (j = 0; j < allChildren.length; j++) {
-        all.push(allChildren[j]);
+      childNodes = nodes[i].querySelectorAll('[mk-html]');
+      for (j = 0; j < childNodes.length; j++) {
+        all.push(childNodes[j]);
       }
-      all.push(nodes[i]);
     }
     for (i = 0; i < all.length; i++) {
       node = all[i];
@@ -1479,17 +1523,18 @@ matreshka_dir_core_bindings_parsebindings = function (core, sym, initMK, util) {
             key = keys[0];
           } else {
             key = core.randomString();
-            core.linkProps(object, key, keys, function () {
-              var v = attrValue;
-              keys.forEach(function (_key) {
-                v = v.replace(new RegExp('{{' + _key + '}}', 'g'), object[sym].special[_key].value);
-              });
-              return v;
-            }, true, { hideProperty: true });
+            (function (keys) {
+              core.linkProps(object, key, keys, function () {
+                var v = attrValue;
+                keys.forEach(function (_key) {
+                  v = v.replace(new RegExp('{{' + _key + '}}', 'g'), object[sym].special[_key].value);
+                });
+                return v;
+              }, true, { hideProperty: true });
+            }(keys));
           }
           if ((attrName == 'value' && node.type != 'checkbox' || attrName == 'checked' && node.type == 'checkbox') && core.lookForBinder(node)) {
-            node.removeAttribute(attrName);
-            core.bindNode(object, key, node);
+            core.bindNode(object, key, node);  //node.removeAttribute(attrName);
           } else {
             core.bindNode(object, key, node, {
               setValue: function (v) {
