af8c822dd93a8a3a3c8b0278868b9e9bff394532
Andrey Gubanov <bookfast.org@gmail.com>
2015-10-23 16:00:51 +0300

    added publish hook for gitter

--- src/core/events/trigger.js
+++ src/core/events/trigger.js
@@ -4,6 +4,30 @@ define([
 	'matreshka_dir/core/util/common'
 ], function(core, sym, utils) {
 	"use strict";
+
+	// TODO
+	var triggerDOMEvent = function(el, name, args) {
+		var event;
+		if(typeof Event !== 'undefined' && !el.fireEvent) {
+			event = new Event(name);
+			event.mkArgs = args;
+			el.dispatchEvent(event);
+		} else if(document.createEvent) {
+			event = document.createEvent('Event');
+			event.initEvent(name, true, true);
+			event.mkArgs = args;
+			el.dispatchEvent(event);
+		} else if(el.fireEvent) {
+			event = document.createEventObject();
+			event.mkArgs = args;
+			el.fireEvent('on' + name, event);
+		} else {
+			throw Error('Cannot trigger DOM event');
+		}
+
+		return event;
+	};
+
 	core.trigger = function(object, names) {
 		var allEvents = object && typeof object == 'object' && object[sym] && object[sym].events,
 			args, i, j, l, events, ev;
