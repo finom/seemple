d2654de28ed8d7aa2a68edafa764d93fef8dcfa1
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2016-01-24 21:56:49 +0200

    rewrite Class function

--- src/xclass.js
+++ src/xclass.js
@@ -1,13 +1,49 @@
 define(function() {
 	"use strict";
-	var ie = typeof document != 'undefined' ? document.documentMode : null,
-		ie8 = ie == 8;
+	var ie = typeof document != 'undefined' ? document.documentMode : null;
 
-	if(ie && ie < 8) {
-		throw Error('Internet Explorer ' + ie + ' doesn\'t support Class function');
+	if(ie && ie < 9) {
+		throw Error('Internet Explorer ' + ie + ' isn\'t supported');
 	}
 
-	var Class = function(prototype, staticProps) {
+	function Class(prototype, staticProps) {
+		var Constructor = prototype.constructor !== Object
+			? prototype.constructor : function EmptyConstructor() {},
+			Parent = prototype['extends'] = prototype['extends'] || prototype.extend,
+			proto,
+			typeofParent,
+			key;
+
+		if(Parent) {
+			typeofParent = typeof Parent;
+			if(typeofParent != 'function') {
+				throw Error('Cannot extend ' + typeofParent);
+			}
+
+			proto = Object.create(Parent.prototype);
+
+			if(Object.assign) {
+				Object.assign(proto, prototype);
+			} else {
+				for(var key in prototype) {
+					proto[key] = prototype[key];
+				}
+			}
+		} else {
+			proto = prototype;
+		}
+
+		Constructor.prototype = proto;
+
+		if (this instanceof Class) {
+			return new Constructor();
+		} else {
+			return Constructor;
+		}
+	};
+
+
+	/*var Class = function(prototype, staticProps) {
 		var realConstructor,
 			constructor = prototype.constructor !== Object
 				? prototype.constructor : function EmptyConstructor() {},
@@ -19,37 +55,11 @@ define(function() {
 
 		delete prototype.extend;
 
-		if (ie8) {
-			prototype.prototype = null;
-			prototype.constructor = null;
-			constructor = function() {
-				if (this instanceof constructor) {
-					var r = new XDomainRequest(),
-						p;
-					for (p in constructor.prototype)
-						if (p !== 'constructor') {
-							r[p] = constructor.prototype[p];
-						}
-					r.hasOwnProperty = constructor.prototype.hasOwnProperty;
-					realConstructor.apply(r, arguments);
-
-					return r;
-				} else {
-					realConstructor.apply(this, arguments);
-				}
-			};
-
-			prototype.constructor = constructor;
-			constructor.prototype = constructor.fn = prototype;
-			constructor.parent = parent;
-			extend && Class.IEInherits(constructor, extend);
-		} else {
 			prototype.constructor = constructor;
 			constructor.prototype = constructor.fn = prototype;
 			constructor.parent = parent;
 
 			extend && Class.inherits(constructor, extend);
-		}
 
 		if(staticProps && typeof staticProps == 'object') {
 			for (key in staticProps) {
@@ -79,54 +89,10 @@ define(function() {
 			Child.prototype[Symbol.iterator] = prototype[Symbol.iterator];
 		}
 
-		Child.prototype.instanceOf = function(_Class) {
-			return this instanceof _Class;
-		};
-	};
 
-	Class.IEInherits = function(Child, Parent) {
-		var childHasOwn = Child.prototype.hasOwnProperty,
-			childConstructor = Child.prototype.constructor,
-			parentHasOwn,
-			objectHasOwn = Object.prototype.hasOwnProperty;
+	};*/
 
-		while (Parent) {
-			parentHasOwn = parentHasOwn || Parent.prototype.hasOwnProperty;
-			Child.prototype = (function(pp, cp) { // extending
-				var o = {},
-					i;
-				for (i in pp) {
-					o[i] = pp[i];
-				}
-				for (i in cp) {
-					o[i] = cp[i];
-				}
-				return o;
-			})(Parent.prototype, Child.prototype);
-			Parent = Parent.prototype && Parent.prototype['extends'] && Parent.prototype['extends'].prototype;
-		}
 
-		if (childHasOwn !== objectHasOwn) {
-			Child.prototype.hasOwnProperty = childHasOwn;
-		} else if (parentHasOwn !== objectHasOwn) {
-			Child.prototype.hasOwnProperty = parentHasOwn;
-		}
-
-		Child.prototype.constructor = childConstructor;
-
-		Child.prototype.instanceOf = function(_Class) {
-			var PossibleParent = Child;
-			while (PossibleParent) {
-				if (PossibleParent === _Class) {
-					return true;
-				}
-				PossibleParent = PossibleParent.prototype['extends'];
-			}
-			return false;
-		};
-	};
-
-	Class.isXDR = ie8;
 
 	return Class;
 });
