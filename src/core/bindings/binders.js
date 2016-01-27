4f09579dfe845016b8f3d5bedcbda9770938d738
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2016-01-27 14:39:34 +0200

    minor

--- src/core/bindings/binders.js
+++ src/core/bindings/binders.js
@@ -83,21 +83,21 @@ define([
 
 					_this.classList
 						? _this.classList[add ? 'add' : 'remove'](className)
-						: add ? addClass(_this, className) : removeClass(_this, className)
-					}
+						: add ? addClass(_this, className) : removeClass(_this, className);
+				}
 			};
 
 			// @IE9
 			// thanks to Iliya Kantor
 			function addClass(o, c){
-				var re = new RegExp("(^|\\s)" + c + "(\\s|$)", "g")
-				if (re.test(o.className)) return
-				o.className = (o.className + " " + c).replace(/\s+/g, " ").replace(/(^ | $)/g, "")
+				var re = new RegExp("(^|\\s)" + c + "(\\s|$)", "g");
+				if (re.test(o.className)) return;
+				o.className = (o.className + " " + c).replace(/\s+/g, " ").replace(/(^ | $)/g, "");
 			}
 
 			function removeClass(o, c){
-				var re = new RegExp("(^|\\s)" + c + "(\\s|$)", "g")
-				o.className = o.className.replace(re, "$1").replace(/\s+/g, " ").replace(/(^ | $)/g, "")
+				var re = new RegExp("(^|\\s)" + c + "(\\s|$)", "g");
+				o.className = o.className.replace(re, "$1").replace(/\s+/g, " ").replace(/(^ | $)/g, "");
 			}
 
 			function hasClass(o, c) {
