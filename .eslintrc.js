ad77c371d4dce3fc4b43c903cd2f745efee8b565
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2016-07-28 16:23:28 +0300

    add eslint.js to git index

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    root: true,
+    "extends": "airbnb",
+    "plugins": ["output-todo-comments"],
+    "rules": {
+
+        "indent": ["error", 4, { "SwitchCase": 1 }],
+        "no-var": "error",
+        "no-else-return": 0,
+        "prefer-rest-params": 0,
+        "new-cap": ["error", {"capIsNewExceptions": ["Class"]}],
+        "comma-dangle": 0,
+        'output-todo-comments/output-todo-comments': [
+            'warn', {
+                'terms': ['todo'],
+                'location': 'start',
+            }
+        ]
+    },
+    "env": {
+        "jasmine": true
+    },
+    "globals": {
+        "nofn": true
+    }
+}
