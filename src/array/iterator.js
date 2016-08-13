2807e3c66a0d884237fe7aff6d5a45beb5560cfd
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2016-08-13 23:48:38 +0300

    add array iterator to git index

---
+++ src/array/iterator.js
@@ -0,0 +1,21 @@
+import initMK from '../_core/init';
+
+// Symbol.iterator of Matreshka.Array instances
+export default function matreshkaArrayIterator() {
+    let i = 0;
+
+    return {
+        next: () => {
+            if (i > this.length - 1) {
+                return {
+                    done: true
+                };
+            }
+
+            return {
+                done: false,
+                value: this[i++]
+            };
+        }
+    };
+}
