52c3649c732a61378515f0bfea239cf3cfb59340
Andrey Gubanov (his digital copy) <andrey.a.gubanov@gmail.com>
2016-09-01 20:38:13 +0300

    feat: Allow to use a function in static methods

--- src/_helpers/checkobjecttype.js
+++ src/_helpers/checkobjecttype.js
@@ -4,7 +4,7 @@ import matreshkaError from './matreshkaerror';
 export default function checkObjectType(object, method) {
     const typeofObject = object === null ? 'null' : typeof object;
 
-    if (typeofObject !== 'object') {
+    if (typeofObject !== 'object' && typeofObject !== 'function') {
         throw matreshkaError('common:object_type', {
             object,
             method
