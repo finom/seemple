74d4cf7f4d930d418ced1a3dfe14e5772c516727
Andrey Gubanov (his digital copy) <andrey.a.gubanov@gmail.com>
2016-09-06 18:28:18 +0300

    fix: Don't call setValue if a property is changed via getValue of the same binder

--- src/bindnode/_createobjecthandler.js
+++ src/bindnode/_createobjecthandler.js
@@ -3,17 +3,17 @@ export default function createObjectHandler({
     node,
     propDef,
     binder,
-    bindingOptions,
-    eventOptions
+    bindingOptions
 }) {
-    return function objectHandler() {
+    return function objectHandler(eventOptions = {}) {
         const { value } = propDef;
         const { onChangeValue, changedNode, binder: evtBinder } = eventOptions;
         const { setValue } = binder;
         // dirty hack for https://github.com/matreshkajs/matreshka/issues/19
         const dirtyHackValue = onChangeValue === 'string' && typeof value === 'number'
-            ? String(value) : value;
+            ? `${value}` : value;
 
+        // don't call setValue if a property is changed via getValue of the same binder
         if (changedNode === node && onChangeValue === dirtyHackValue && evtBinder === binder) {
             return;
         }
