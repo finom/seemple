87ea005bf5a7b9ba88a73712421908692cecb6c6
Andrey Gubanov (his digital copy) <andrey.a.gubanov@gmail.com>
2016-10-07 17:26:13 +0300

    chore: Add Twitter bot

---
+++ tools/twit-release.js
@@ -0,0 +1,20 @@
+const Twit = require('twit');
+const { version } = require('../package');
+
+const { env } = process;
+
+const twit = new Twit({
+    consumer_key: env.TWITTER_CONSUMER_KEY,
+    consumer_secret: env.TWITTER_CONSUMER_SECRET,
+    access_token: env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+twit.post('statuses/update', {
+    status: `Hello humans! Matreshka v${version} is released`
+        + ` https://github.com/matreshkajs/matreshka/releases/tag/v${version}`
+}, (err) => {
+    if (err) {
+        console.log(`Something gone wrong with Twitter bot: ${err}`); // eslint-disable-line no-console
+    }
+});
