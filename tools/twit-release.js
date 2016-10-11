5f87d4e6a250211dbc3784a616df9e0c3d8a5c92
Andrey Gubanov (his digital copy) <andrey.a.gubanov@gmail.com>
2016-10-11 15:58:48 +0300

    chore: Add emoji to twit-release script

--- tools/twit-release.js
+++ tools/twit-release.js
@@ -11,7 +11,7 @@ const twit = new Twit({
 });
 
 twit.post('statuses/update', {
-    status: `Hello humans! Matreshka.js v${version} is released`
+    status: `🎉 Hello humans! Matreshka.js v${version} is released`
         + ` https://github.com/matreshkajs/matreshka/releases/tag/v${version}`
 }, (err) => {
     if (err) {
