99f280441b4d343b7ac213320c4f9c83683dde8a
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2018-04-21 16:58:01 +0300

    fix: Use common-binders package as a dependency

--- src/binders/index.js
+++ src/binders/index.js
@@ -1,17 +1,13 @@
-import html from './html';
-import display from './display';
-import className from './classname';
-import prop from './prop';
-import attr from './attr';
+import {
+    html, display, className, prop, attr,
+    text, style, dataset, existence
+} from 'common-binders';
+
 import input from './input';
 import output from './output';
 import textarea from './textarea';
 import select from './select';
 import progress from './progress';
-import text from './text';
-import style from './style';
-import dataset from './dataset';
-import existence from './existence';
 
 export {
     html,
