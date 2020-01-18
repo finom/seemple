da8a3864f6d16abead00eba3efc19a901661e7c8
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2020-01-18 22:12:59 +0200

    chore: Minors

--- test/post-publish/post-publish.js
+++ test/post-publish/post-publish.js
@@ -1,7 +1,24 @@
 const { execSync } = require('child_process');
+const { JSDOM } = require('jsdom');
+var expect = require('expect.js');
 
-execSync('npm i --no-package-lock', { cwd: __dirname });
+
+execSync('rm -rf node_modules && npm i --no-package-lock', { cwd: __dirname });
+
+global.window = new JSDOM('<!doctype html><html><body><form><input name="foo"></form></body></html>', {
+  url: 'http://localhost'
+}).window;
+
+global.document = global.window.document;
 
 const Seemple = require('seemple');
 const parseForm = require('seemple-parse-form');
-const router = require('seemple-router');
+const SeempleRouter = require('seemple-router');
+
+// check if seemple itself is OK
+const seemple = new Seemple();
+seemple.b = 3;
+seemple.calc('a', 'b', (b) => b * 2);
+expect(seemple.a).to.eql(6);
+
+parseForm(global.document.querySelector('form'));
